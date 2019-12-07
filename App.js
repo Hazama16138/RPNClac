c1e656e09941936dc84e36e222c2b62a1d932c8b
hazama16138 <like_apple0302@ezweb.ne.jp>
2019-12-07 14:37:05 +0900

    ボタン機能の作成

--- App.js
+++ App.js
@@ -19,59 +19,236 @@ const CalcButton = (props) => {
 		>
 			<Text style={styles.calcButtonText}>{props.label}</Text>
 		</TouchableOpacity>
-	)
-}
+	);
+};
+
+const CalcButtons = (props) => {
+	return(
+		<React.Fragment>
+			{props.buttons.map(button => {
+				return (
+					<CalcButton
+						key={button.label}
+						flex={button.flex}
+						label={button.label}
+						btnEvent={button.btnEvent}
+					/>
+				);
+			})}
+		</React.Fragment>
+	);
+};
 
 export default class App extends React.Component {
+	buttons = [
+		[
+			{
+				label: 'AC',
+				flex: 2,
+				btnEvent: () => {this.acButton()},
+			},
+			{
+				label: 'C',
+				btnEvent: () => {this.cButton()},
+			},
+			{
+				label: '+',
+				btnEvent: () => {this.calcButton('+')},
+			}
+		],
+		[
+			{
+				label: '7',
+				btnEvent: () => {this.valueButton('7')},
+			},
+			{
+				label: '8',
+				btnEvent: () => {this.valueButton('8')},
+			},
+			{
+				label: '9',
+				btnEvent: () => {this.valueButton('9')},
+			},
+			{
+				label: '-',
+				btnEvent: () => {this.calcButton('-')},
+			}
+		],
+		[
+			{
+				label: '4',
+				btnEvent: () => {this.valueButton('4')},
+			},
+			{
+				label: '5',
+				btnEvent: () => {this.valueButton('5')},
+			},
+			{
+				label: '6',
+				btnEvent: () => {this.valueButton('6')},
+			},
+			{
+				label: '*',
+				btnEvent: () => {this.calcButton('*')},
+			},
+		],
+		[
+			{
+				label: '1',
+				btnEvent: () => {this.valueButton('1')},
+			},
+			{
+				label: '2',
+				btnEvent: () => {this.valueButton('2')},
+			},
+			{
+				label: '3',
+				btnEvent: () => {this.valueButton('3')},
+			},
+		],
+		[
+			{
+				label: '0',
+				btnEvent: () => {this.valueButton('0')},
+			},
+			{
+				label: '.',
+				btnEvent: () => {this.valueButton('.')},
+			},
+			{
+				label: '/',
+				btnEvent: () => {this.calcButton('/')},
+			},
+		],
+		[
+			{
+				label: 'Enter',
+				btnEvent: () => {this.enterButton()},
+			},
+		]
+	];
+	
+	constructor(props) {
+		super(props);
+		this.state = {
+			results: [],
+			current: "0",
+			dotInputed: false,
+			afterValueButton: false,
+		};
+	};
+	
 	valueButton = (value) => {
-		
-	}
+		let currentString = this.state.current;
+		const dotInputed = this.state.dotInputed;
+		let newDotInputed = dotInputed;
+		if (value == ".") {
+			if (!dotInputed) {
+				currentString = currentString + value;
+				newDotInputed = true;
+			}
+		} else if (currentString == "0") {
+			currentString = value;
+		} else {
+			currentString = currentString + value;
+		}
+		this.setState({current: currentString, dotInputed: newDotInputed, afterValueButton: true});
+	};
 	
 	enterButton = () => {
-		
-	}
+		let newValue = NaN;
+		if (this.state.dotInputed) {
+			newValue = parseFloat(this.state.current);
+		} else {
+			newValue = parseInt(this.state.current, 10);
+		}
+		if (isNaN(newValue)) {
+			return;
+		}
+		let results = this.state.results;
+		results.push(newValue);
+		this.setState({current: "0", dotInputed: false, reuslts: results, afterValueButton: false});
+	};
 	
 	calcButton = (value) => {
-		
-	}
+		if (this.state.results.length < 2) {
+			return;
+		}
+		if (this.state.afterValueButton == true) {
+			return;
+		}
+		let newResults = this.state.results;
+		const target2 = newResults.pop();
+		const target1 = newResults.pop();
+		let newValue = null;
+		switch (value) {
+			case '+':
+				newValue = target1 + target2;
+				break;
+			case '-':
+				newValue = target1 - target2;
+				break;
+			case '*':
+				newValue = target1 * target2;
+				break;
+			case '/':
+				newValue = target1 / target2;
+				if (!isFinite(newValue)) {
+					newValue = null;
+				}
+				break;
+			default:
+				break;
+		}
+		if (newValue == null) {
+			return;
+		}
+		newResults.push(newValue);
+		this.setState({current: "0", dotInputed: false, results: newResults, afterValueButton: false});
+	};
 	
 	acButton = (value) => {
-		
-	}
+		this.setState({current: "0", dotInputed: false, results: [], afterValueButton: false});
+	};
 	
 	cButton = () => {
-		
-	}
+		this.setState({current: "0", dotInputed: false, afterValueButton: false});
+	};
 	
 	render() {
 	  return (
 	    <View style={styles.container}>
 	      <View style={styles.results}>
-					<View style={styles.resultLine}>	
+					<View style={styles.resultLine}>
 					</View>
 					<View style={styles.resultLine}>	
+						<Text>{this.state.current}</Text>
 					</View>
 					<View style={styles.resultLine}>	
+						<Text>{this.state.results.join(' ')}</Text>
 					</View>
 				</View>
 				<View style={styles.buttons}>
 					<View style={styles.buttonsLine}>
-						<CalcButton flex={2} label={'AC'} btnEvent={() => this.acButton()} />
-						<CalcButton label={'C'} btnEvent={() => this.cButton()} />
-						<CalcButton label={'+'} btnEvent={() => this.calcButton('+')} />
+						<CalcButtons buttons={this.buttons[0]} />
 					</View>
 					<View style={styles.buttonsLine}>
+						<CalcButtons buttons={this.buttons[1]} />
 					</View>
 					<View style={styles.buttonsLine}>
+						<CalcButtons buttons={this.buttons[2]} />
 					</View>
 					<View style={styles.lastButtonLinesContainer}>
 						<View style={styles.twoButtonLines}>
 							<View style={styles.buttonsLine}>
+								<CalcButtons buttons={this.buttons[3]} />
 							</View>
 							<View style={styles.buttonsLine}>
+								<CalcButtons buttons={this.buttons[4]} />
 							</View>
 						</View>
 						<View style={styles.enterButtonContainer}>
+							<CalcButtons buttons={this.buttons[5]} />
 						</View>
 					</View>
 				</View>	
